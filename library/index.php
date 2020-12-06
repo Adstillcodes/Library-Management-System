ef0c3808ac77890e24d6470c5e26eed65112d80b
Anvit3 <70889070+Anvit3@users.noreply.github.com>
2020-12-06 06:43:54 -0800

    Add files via upload

---
+++ library/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Library Management System</title>
+    
+</head>
+    
+<body>
+    <section class="text-gray-700 body-font">
+  <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="assets/img/logo.png">
+    <div class="text-center lg:w-2/3 w-full">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to Library Management System</h1>
+      <p class="mb-8 leading-relaxed">Library management system created by Anvit Deshpande</p>
+      <div class="flex justify-center">
+       <a href='adminlogin.php'><button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login as admin</button></a>
+          
+        <a href='index2.php'><button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login as student</button></a>
+          
+      </div>
+    </div>
+  </div>
+</section>
+          
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
